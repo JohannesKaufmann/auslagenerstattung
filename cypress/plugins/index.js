6db856aa37adbee1a7f4dfe3c122a460fb2b675a
Johannes Kaufmann <johannes@joina.de>
2022-05-21 20:18:23 +0200

    add cypress testing

---
+++ cypress/plugins/index.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+const { readPdf } = require("./read-pdf");
+const path = require("path");
+const fs = require("fs");
+
+const downloadDirectory = path.join(__dirname, "..", "downloads");
+
+const findPDF = (filename) => {
+  const contents = fs.existsSync(filename);
+  return contents;
+};
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  on("task", {
+    readPdf,
+    findPDF,
+  });
+
+  on("before:browser:launch", (browser, options) => {
+    if (browser.family === "chromium") {
+      if (!options.preferences.default) {
+        options.preferences.default = {};
+      }
+      options.preferences.default["download"] = {
+        default_directory: downloadDirectory,
+      };
+      return options;
+    }
+    if (browser.family === "firefox") {
+      options.preferences["browser.download.dir"] = downloadDirectory;
+      options.preferences["browser.download.folderList"] = 2;
+      options.preferences["browser.helperApps.neverAsk.saveToDisk"] =
+        "application/pdf";
+      return options;
+    }
+  });
+};
