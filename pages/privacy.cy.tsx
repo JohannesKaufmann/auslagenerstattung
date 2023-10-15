7dcc5a5fd54bb43ae3e4a96a08e9abac8b78d646
Johannes Kaufmann <johannes@joina.de>
2023-10-15 11:56:47 +0200

    remove cypress component test

--- pages/privacy.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PrivacyPage from './privacy'
-
-describe('<PrivacyPage />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<PrivacyPage />)
-  })
-})
